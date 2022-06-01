50274c7b5dbac2cfe5b778a519f1ea216b5153f8
Yuriy Chabaniuk <yuriy.chabaniuk@sigmetix.com>
2022-06-01 20:09:06 +0300

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 if [ -z "$1" ]; then
-  DIR_TO_SCAN="./"
+  DIR_TO_SCAN="."
 else
   DIR_TO_SCAN="$1"
 fi
 
 echo "Running: phplint $DIR_TO_SCAN"
 
-phplint "$DIR_TO_SCAN"
+phplint "ezcollege"
